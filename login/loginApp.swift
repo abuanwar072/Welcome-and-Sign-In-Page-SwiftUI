c0da7ad9a8a428ec26ec5582fd45b84240847206
Abu Anwar <abuanwar072@gmail.com>
2021-04-23 09:13:41 +0600

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Abu Anwar MD Abdullah on 23/4/21.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
